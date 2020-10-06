0ccb0b2ffb303ba840621b60d7f6e8e983c2720b
Rick Teachey <ricky@teachey.org>
2020-10-06 00:39:11 -0400

    Add type hints to interpolate.py

--- asce7/interpolate.py
+++ asce7/interpolate.py
@@ -1,4 +1,9 @@
 """Interpolation helpers"""
+from decimal import Decimal
+from numbers import Real
+from typing import Sequence, Callable, Sized, Iterable, TypeVar
+
+T = TypeVar("T", Real, Decimal, float)
 
 
 class InterpolationError(Exception):
@@ -9,27 +14,29 @@ class LengthError(Exception):
     pass
 
 
-def length_match_guard(*collections):
+def length_match_guard(*collections: Sized) -> None:
     """Check is collections are the same length"""
     if len({len(c) for c in collections}) != 1:
         raise LengthError("length mismatch")
 
 
-def linearly_interpolate(v, x1, x2, y1, y2):
+def linearly_interpolate(v: T, x1: T, x2: T, y1: T, y2: T) -> T:
     """Linearly interpolate along a two point line"""
     return y1 + (v - x1) * (y2 - y1) / (x2 - x1)
 
 
-def linear_interpolator(x1, x2, y1, y2):
+def linear_interpolator(
+    x1: T, x2: T, y1: T, y2: T
+) -> Callable[[T], T]:
     """Create an interpolating function using the provided xs and ys"""
 
-    def interpolator(v):
+    def interpolator(v: T) -> T:
         return linearly_interpolate(v, x1, x2, y1, y2)
 
     return interpolator
 
 
-def piecewise_index(v, numbers):
+def piecewise_index(v: T, numbers: Sequence[T]) -> int:
     """Find first index where v is the middle value"""
     try:
         return next(
@@ -41,9 +48,11 @@ def piecewise_index(v, numbers):
         raise InterpolationError("Value does not fall in any of the ranges")
 
 
-def piecewise_line_coordinates_from_index(idx, x1s, x2s, y1s, y2s):
-    """Find the x and y coordinates that define the proper piecewise line
-    in a set of curves for at the provided index, idx
+def piecewise_line_coordinates_from_index(
+    idx: int, x1s: Iterable[T], x2s: Iterable[T], y1s: Iterable[T], y2s: Iterable[T]
+) -> tuple[T, T, T, T]:
+    """The x and y coordinates that define the proper piecewise line
+    in a set of curves at the provided index, idx
     """
     x1, x2, y1, y2 = next(
         tup for i, tup in enumerate(zip(x1s, x2s, y1s, y2s)) if i == idx
@@ -51,7 +60,7 @@ def piecewise_line_coordinates_from_index(idx, x1s, x2s, y1s, y2s):
     return x1, x2, y1, y2
 
 
-def piecewise_line_coordinates_from_value(v, xs, ys):
+def piecewise_line_coordinates_from_value(v: T, xs: Sequence[T], ys: Sequence[T]) -> tuple[T, T, T, T]:
     """Find the x and y coordinates that define the proper piecewise line in a set of curves
     for the provided value, v
     """
@@ -67,12 +76,12 @@ def piecewise_line_coordinates_from_value(v, xs, ys):
     return x1, x2, y1, y2
 
 
-def piecewise_interpolate(v, xs, ys):
+def piecewise_interpolate(v: T, xs: Sequence[T], ys: Sequence[T]):
     """Linearly interpolate across piecewise curves"""
     return linearly_interpolate(v, *piecewise_line_coordinates_from_value(v, xs, ys))
 
 
-def piecewise_interpolator(xs, ys):
+def piecewise_interpolator(xs: Sequence[T], ys: Sequence[T]) -> Callable[[T], T]:
     """Create an interpolating function across piecewise curves using the provided xs and ys"""
     try:
         length_match_guard(xs, ys)
@@ -82,7 +91,7 @@ def piecewise_interpolator(xs, ys):
     piecewise_section_coordinates = list(zip(x1s, x2s, y1s, y2s))
     interpolators = [linear_interpolator(*tup) for tup in piecewise_section_coordinates]
 
-    def interpolate(v):
+    def interpolate(v: T) -> T:
         idx = piecewise_index(v, xs)
         interpolator = interpolators[idx]
         return interpolator(v)
