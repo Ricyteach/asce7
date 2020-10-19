a67f58f5111e5086f19fd473f02fa75de2be9086
Rick Teachey <ricky@teachey.org>
2020-10-19 01:14:22 -0400

    Revamped figure module testing, many docstrings added

--- asce7/figure.py
+++ asce7/figure.py
@@ -1,3 +1,4 @@
+"""For implementing lookups in figures provided by the building code."""
 from dataclasses import dataclass
 from decimal import Decimal
 from numbers import Real
@@ -9,22 +10,22 @@ Number = TypeVar("Number", Real, Decimal, float)
 NumberFunc = Callable[..., Number]
 
 
-def return_value(value: T) -> T:
+def _return_value(value: T) -> T:
     """Just return the value."""
     return value
 
 
-class VariableSeq(list):
+class _VariableSeq(list):
     """The independent or dependent variables of a curve
 
     Provides the values that make the curve, and functions to convert the values to and from the axis value (which may
-    not be the same as the curve value).
+    or may not be the same as the curve value).
     """
 
-    def __new__(cls, value, /, from_axis: NumberFunc = return_value, from_curve: NumberFunc = return_value):
+    def __new__(cls, value, /, from_axis: NumberFunc = _return_value, from_curve: NumberFunc = _return_value):
         return super().__new__(cls, value)
 
-    def __init__(self, value, /, from_axis: NumberFunc = return_value, from_curve: NumberFunc = return_value):
+    def __init__(self, value, /, from_axis: NumberFunc = _return_value, from_curve: NumberFunc = _return_value):
         super().__init__(value)
         self.from_axis = from_axis
         self.from_curve = from_curve
@@ -32,10 +33,11 @@ class VariableSeq(list):
 
 @dataclass(repr=False)
 class Curve:
+    """Piecewise curves making up a figure."""
 
     def __init__(self, independent: Iterable[Number] = None, dependent: Iterable[Number] = None):
-        self.independent = VariableSeq(independent if independent is not None else [])
-        self.dependent = VariableSeq(dependent if independent is not None else [])
+        self.independent = _VariableSeq(independent if independent is not None else [])
+        self.dependent = _VariableSeq(dependent if independent is not None else [])
         self.interpolate = piecewise_interpolator([self.independent.from_axis(i) for i in self.independent],
                                                   [self.dependent.from_axis(d) for d in self.dependent])
 
@@ -47,11 +49,44 @@ class Curve:
         independent_value = self.interpolate(dependent_value)
         return self.dependent.from_curve(independent_value)
 
+    @property
+    def from_dependent(self) -> NumberFunc:
+        return self.dependent.from_curve
+
+    @from_dependent.setter
+    def from_dependent(self, func: NumberFunc):
+        self.dependent.from_curve = func if func is not None else _return_value
+
+    @property
+    def to_dependent(self) -> NumberFunc:
+        return self.dependent.from_axis
+
+    @to_dependent.setter
+    def to_dependent(self, func: NumberFunc):
+        self.dependent.from_axis = func if func is not None else _return_value
+
+    @property
+    def from_independent(self) -> NumberFunc:
+        return self.independent.from_curve
+
+    @from_independent.setter
+    def from_independent(self, func: NumberFunc):
+        self.independent.from_curve = func if func is not None else _return_value
+
+    @property
+    def to_independent(self) -> NumberFunc:
+        return self.independent.from_axis
+
+    @to_independent.setter
+    def to_independent(self, func: NumberFunc):
+        self.independent.from_axis = func if func is not None else _return_value
+
 
 class Figure:
+    """An ASCE 7 building code figure."""
 
-    def __init__(self, **curves: tuple[Sequence[Real], Sequence[Real]]):
-        vars(self).update(**{k:Curve(*c) for k,c in curves.items()})
+    def __init__(self, **curves: tuple[Sequence[Number], Sequence[Number]]):
+        vars(self).update(**{k:Curve(*c) for k, c in curves.items()})
 
     def __repr__(self):
         return f"{type(self).__name__}({', '.join(f'{k}={v!r}' for k, v, in vars(self).items())})"
--- tests/test_figure.py
+++ tests/test_figure.py
@@ -1,10 +1,179 @@
-from asce7.figure import Figure
+import math
+from decimal import Decimal
+
+from asce7.figure import Figure, Curve, _return_value
+from asce7.interpolate import InterpolationError
 import pytest
+from pytest import approx
 
 
-@pytest.mark.parametrize("figure, r_str", [
-    (Figure(), "Figure()"),
-    (Figure(a=([], [])), "Figure(a=([], []))"),
+NAMES = list("ab")
+
+
+# for building curve objects, testing lookups, testing errors
+XY_LISTS, DEPENDENTS, ANSWERS, ERRORS = zip(
+    ((["0.1", "0.6"], ["0.0", "5.0"]), "0.3", 2, "0.0"),
+    ((["0.1", "-0.5"], ["5.0", "11.0"]), "-0.1", 7, "-0.6"),
+)
+
+
+# for testing figure objects
+FIGURE_KWARGS, REPRS = zip(
+    ({}, "Figure()"),
+    ({"a": ([], [])}, "Figure(a=([], []))"),
+    (dict(zip(NAMES, XY_LISTS)), "Figure(a=(['0.1', '0.6'], ['0.0', '5.0']), b=(['0.1', '-0.5'], ['5.0', '11.0']))"),
+)
+
+
+@pytest.fixture(scope = "module", params = [float, Decimal])
+def curve_value_type(request):
+    """Convert string values used to make curves (see above)"""
+    return request.param
+
+
+@pytest.fixture(scope = "module")
+def curves(curve_value_type):
+    """List of all the curve objects to be tested"""
+    return [Curve(*([curve_value_type(v) for v in lst] for lst in tup)) for tup in XY_LISTS]
+
+
+@pytest.fixture(scope = "module", params = range(len(XY_LISTS)))
+def curve_no(request):
+    """For running tests on each Curve object individually."""
+    return request.param
+
+
+@pytest.fixture(scope = "module", params = range(len(FIGURE_KWARGS)))
+def figure_no(request):
+    """For running tests on each Figure object individually."""
+    return request.param
+
+
+@pytest.fixture(scope = "module", params = range(2, len(FIGURE_KWARGS)))
+def figure_no_nonempty(request):
+    """For running tests only on the non-empty Figure objects individually."""
+    return request.param
+
+
+@pytest.fixture(scope = "module")
+def figure(figure_no):
+    """Figure objects to be tested"""
+    return Figure(**FIGURE_KWARGS[figure_no])
+
+
+@pytest.fixture(scope = "function")
+def figure_nonempty(figure_no_nonempty):
+    """Figure objects to be tested that have curve members"""
+    return Figure(**FIGURE_KWARGS[figure_no_nonempty])
+
+
+@pytest.fixture(scope = "module")
+def fixture_repr(figure_no):
+    """For checking the figure reprs"""
+    return REPRS[figure_no]
+
+
+@pytest.fixture(scope = "module")
+def curve(curve_no, curves):
+    """Curve objects to be tested"""
+    return curves[curve_no]
+
+
+@pytest.fixture(scope = "module")
+def answer(curve_no):
+    """Answers to be looked up"""
+    return ANSWERS[curve_no]
+
+
+@pytest.fixture(scope = "module")
+def dependent(curve_no, curve_value_type):
+    """Variable to be looked up"""
+    return curve_value_type(DEPENDENTS[curve_no])
+
+
+@pytest.fixture(scope = "module")
+def error_value(curve_no, curve_value_type):
+    """Lookups to be checked that results in errors"""
+    return curve_value_type(ERRORS[curve_no])
+
+
+def test_figure_repr(figure: Figure, fixture_repr: str):
+    """Check figure display"""
+    assert repr(figure) == fixture_repr
+
+
+def test_lookup(curve, answer, dependent):
+    """Check interpolation"""
+    assert curve.lookup(dependent) == approx(answer)
+
+
+def test_lookup_error(curve, error_value):
+    """Check interpolation error"""
+    with pytest.raises(InterpolationError):
+        curve.lookup(error_value)
+
+
+@pytest.fixture(params = [
+    "from_dependent",
+    "to_dependent",
+    "from_independent",
+    "to_independent",
 ])
-def test_figure_repr(figure, r_str):
-    assert repr(figure) == r_str
+def figure_conversion_method_name(request):
+    """Method names for checking all the conversions"""
+    return request.param
+
+
+@pytest.fixture(params = NAMES)
+def figure_attr(request):
+    """Figure member names"""
+    return request.param
+
+
+@pytest.fixture
+def figure_conversion_method(figure_conversion_method_name, figure_nonempty, figure_attr):
+    """All the conversions to be checked"""
+    curve = getattr(figure_nonempty, figure_attr)
+    return getattr(curve, figure_conversion_method_name)
+
+
+@pytest.fixture(params = [0.001234, 1.1, 3679, Decimal("123.456")])
+def conversion_value(request):
+    """A bunch of values to be converted"""
+    return request.param
+
+
+def test_figure_conversion(figure_conversion_method, conversion_value):
+    """A bunch of conversion tests"""
+    assert figure_conversion_method(conversion_value) == approx(conversion_value)
+
+
+@pytest.fixture(params = [None, _return_value, math.log], ids = ["default", "unchanged", "log"])
+def conversion_func(request):
+    """A bunch of changes to the conversion test"""
+    return request.param if request.param is not None else _return_value
+
+
+@pytest.fixture
+def modify_figure_conversion(figure_conversion_method_name, figure_nonempty, figure_attr, conversion_func):
+    """Execute changes to conversion"""
+    curve = getattr(figure_nonempty, figure_attr)
+    setattr(curve, figure_conversion_method_name, conversion_func)
+
+
+@pytest.fixture
+def figure_conversion_modified(modify_figure_conversion, figure_conversion_method_name, figure_nonempty, figure_attr):
+    """Already modified conversion methods"""
+    curve = getattr(figure_nonempty, figure_attr)
+    return getattr(curve, figure_conversion_method_name)
+
+
+@pytest.fixture
+def converted_value(conversion_value, conversion_func):
+    """Correct answers for tested conversions"""
+    return conversion_func(conversion_value)
+
+
+def test_figure_conversion_modified(figure_conversion_modified, conversion_value, converted_value):
+    """Check modified conversions for correctness"""
+    assert figure_conversion_modified(conversion_value) == approx(converted_value)
