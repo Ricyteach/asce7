19c68ae4f64ecea908a96c70dc5fa5749c69dc5a
Rick Teachey <ricky@teachey.org>
2021-05-13 00:38:25 -0400

    Ct Figure 7.4-1 (incomplete)

--- asce7/v2016/chapter7.py
+++ asce7/v2016/chapter7.py
@@ -1,25 +1,37 @@
 """ASCE 7 Figures"""
 
-from ..exceptions import ASCE7Error
+from asce7.lookup import ParameterStandard
 
 
-def _fig_7_pt_4_dsh_1(C_t, roof_slope_deg):
-    """For determining C_s"""
+def _fig7p4d1():
+    """Figure 29.4-7: Design Wind Loads (All Heights): Rooftop Solar Panels for Enclosed and Partially Enclosed
+    Buildings, Roof θ≤7°
+
+    Nominal Net Pressure Coefficients (Gcrn)nom
+    """
+    fig7p4d1_labels = {'Surface Type': ['slippery', 'other']}
+    fig7p4d1_indexes = {
+        'C_t': (1.0, 1.1, 1.2),  # temperature coefficient
+        'roof slope': [[[5, 70],[30, 70],],  # C_t = 1.0 chart
+                       [[10, 70],[37.5, 70],],  # C_t = 1.1 chart
+                       [[15, 70],[45, 70],],],  # C_t = 1.2 chart
+    }
+    fig7p4d1_dependent_axis = {
+        'C_t = 1.0 chart': [
+            [1.0, 0], [1.0, 0],
+        ],
+        'C_t = 1.1 chart': [
+            [1.0, 0], [1.0, 0],
+        ],
+        'C_t = 1.2 chart': [
+            [1.0, 0], [1.0, 0],
+        ],
+    }
+
+    return ParameterStandard(fig7p4d1_dependent_axis, fig7p4d1_indexes, fig7p4d1_labels)
 
 
-def _get_figure(C_t):
-    """Retrieve correct subfigure for C_s"""
-    if C_t >= 1.2:
-        return _fig_7_dsh_2c
-    elif C_t == 1.1:
-        return _fig_7_dsh_2b
-    elif C_t <= 1.0:
-        return _fig_7_dsh_2a
-    else:
-        raise ASCE7Error(f"{C_s=:.1f} is unsupported in ASCE 7")
-
-
-def _fig_7_dsh_2a(roof_slope_deg):
+def _fig7p3d2a(roof_slope_deg):
     """Retrieve value for C_t<=1.0"""
     _fig_7_dsh_2a_interpolator(roof_slope_deg)
     return interpolate()
