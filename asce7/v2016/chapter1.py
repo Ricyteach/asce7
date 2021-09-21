4c0f5d959785d19514c32977dff6d42fa604c3d5
rteachey <ricky@teachey.org>
2021-09-21 15:26:05 -0400

    implemented outline for chapter1 and chapter2 (except seismic)

--- asce7/v2016/chapter1.py
+++ asce7/v2016/chapter1.py
@@ -2,20 +2,33 @@ from enum import Enum
 import pandas as pd
 
 
-class LoadType(Enum):
+class LoadType(str, Enum):
+    """cf. Sections 1.2.2 and 2.2"""
     dead = "D"
     live = "L"
     snow = "S"
     wind = "W"
     seismic = "E"
+    live_roof = "Lr"
+    rain = "R"
+    dead_ice = "Di"
+    fluid = "F"
+    flood = "Fa"
+    temperature = "T"
     D = "D"
     L = "L"
     S = "S"
     W = "W"
     E = "E"
+    Lr = "Lr"
+    R = "R"
+    Di = "Di"
+    F = "F"
+    Fa = "Fa"
+    T = "T"
 
 
-class RiskCategory(Enum):
+class Risk(str, Enum):
     """Risk Category of Buildings and Other Structures for Flood, Wind, Snow, Earthquake, and Ice Loads.
 
     cf. Table 1.5-1
@@ -26,10 +39,12 @@ class RiskCategory(Enum):
     IV = "IV"
 
 
-_Table_1_pt_5_dsh_2 = pd.DataFrame({LoadType.snow: [0.80, 1.00, 1.10, 1.20],
-                             LoadType.seismic: [1.00, 1.00, 1.25, 1.50]},
-                            index=list(RiskCategory))
+TABLE_1P5D2_Ix = pd.DataFrame({LoadType.snow: [0.80, 1.00, 1.10, 1.20],
+                               LoadType.dead_ice: [0.80, 1.00, 1.15, 1.25],
+                               LoadType.seismic: [1.00, 1.00, 1.25, 1.50]},
+                              index=list(Risk))
 
 
-def importance_factor(load_type):
-    return lambda risk: _Table_1_pt_5_dsh_2[LoadType(load_type)][RiskCategory(risk)]
+def importance_factor(risk, load_type):
+    """cf. Table 1.5-2 Importance Factors"""
+    return TABLE_1P5D2_Ix[LoadType(load_type)][Risk(risk)]
